dac67f2f511853f5e1839706007d40de07da275e
Ezeofor PraiseGod <senitenttechnology@gmail.com>
2025-09-23 14:38:12 +0100

    minor update

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,17 +1,64 @@
 'use client';
 
+import { scrapeAndStoreProduct } from '@/lib/actions';
+import { FormEvent, useState } from 'react';
+
+const isValidAmazonProductUrl = (url: string) => {
+	try {
+		const parsedUrl = new URL(url);
+		const hostName = parsedUrl.hostname;
+
+		// check if hostname contains amazon.com
+
+		if (
+			hostName.includes('amazon.com') ||
+			hostName.includes('amazon.') ||
+			hostName.endsWith('amazon')
+		) {
+			return true;
+		}
+	} catch (err) {
+		return false;
+	}
+};
+
 const Searchbar = () => {
-	const handleSubmit = () => {};
+	const [searchprompt, setSearchprompt] = useState('');
+	const [isLoading, setIsLoading] = useState(false);
+
+	const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+
+		const isValidLink = isValidAmazonProductUrl(searchprompt);
+
+		if (!isValidLink) return alert('Please provide a valid amazon link');
+
+		try {
+			setIsLoading(true);
+
+			// scrape the product page
+			const product = await scrapeAndStoreProduct(searchprompt);
+		} catch (err) {
+			console.log(err);
+		} finally {
+			setIsLoading(false);
+		}
+	};
 
 	return (
 		<form className='flex flex-wrap gap-4 mt-12' onSubmit={handleSubmit}>
 			<input
 				type='text'
+				onChange={(e) => setSearchprompt(e.target.value)}
+				value={searchprompt}
 				placeholder='Enter product link'
 				className='searchbar-input'
 			/>
-			<button type='submit' className='searchbar-btn'>
-				search
+			<button
+				disabled={searchprompt === ''}
+				type='submit'
+				className='searchbar-btn'>
+				{isLoading ? 'searching...' : 'search'}
 			</button>
 		</form>
 	);
---
+++ lib/scrapper/index.ts
@@ -0,0 +1,57 @@
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+import { extractPrice } from '../utils';
+
+export async function scrapeAmazonProduct(url: string) {
+	if (!url) return;
+
+	// BrightData proxy configuration
+	const username = String(process.env.BRIGHT_DATA_USERNAME);
+	const password = String(process.env.BRIGHT_DATA_PASSWORD);
+	const port = 33335;
+	const session_id = (1000000 * Math.random()) | 0;
+
+	const options = {
+		auth: {
+			username: `${username}-session-${session_id}`,
+			password: password,
+		},
+		host: 'brd.superproxy.io',
+		port,
+		rejectUnauthorized: false,
+	};
+
+	try {
+		// Fetch product page
+		const response = await axios.get(url, options);
+		const $ = cheerio.load(response.data);
+
+		// Extract the product title
+		const title = $('#productTitle').text().trim();
+		const currentPrice = extractPrice(
+			$('.priceToPay span.a-price-whole'),
+			$('a.size.base.a-color-price'),
+			$('.a-button-selected .a-color-base'),
+			$('a.size-small')
+		);
+
+		const originalPrice = extractPrice(
+			$('#priceblock_ourprice'),
+			$('.a-price.a-text-price span.a-offscreen'),
+			$('#listPrice'),
+			$('#pricebook_dealprice'),
+			$('.a-size-base.a-color-price')
+		);
+
+		console.log(
+			'Title:',
+			title,
+			'Price:',
+			currentPrice,
+			'Original Price:',
+			originalPrice
+		);
+	} catch (err: any) {
+		throw new Error(`Failed to scrape product: ${err.message}`);
+	}
+}
